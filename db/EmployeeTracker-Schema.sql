24af81c29927164c888a43cb95ed37c21f635db3
Omri <omri.ronen4@gmail.com>
2021-06-08 12:56:34 -0700

    took out the primary key id for employee table because it written twice

--- db/EmployeeTracker-Schema.sql
+++ db/EmployeeTracker-Schema.sql
@@ -27,7 +27,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
